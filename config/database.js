a0cdbb323ea57f3289acd75d4304c1a0e6a27db4
nurik1347 <nurikmirzaev1747@gmail.com>
2024-10-11 18:32:42 +0500

    done

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://nurikmirzaev1747:llzK46UxpWDy23nv@cluster0.m6mtj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        await mongoose.connect('mongodb+srv://Nurik:Nurik1747@cluster0.tbe1j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log('MongoDB connected successfully');
     } catch (err) {
         console.error('MongoDB connection failed:', err);
